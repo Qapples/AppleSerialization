984e8d27bf4b991eff9f9d1e47bd83bafff7c031
qapples <793678@lcps.org>
2021-09-06 19:02:28 -0400

    Introduce JsonElementTypes.cs

---
+++ AppleSerialization/Json/JsonElementTypes.cs
@@ -0,0 +1,12 @@
+namespace AppleSerialization.Json
+{
+    /// <summary>
+    /// Represents the possible variations of Json elements in an enum.
+    /// </summary>
+    public enum JsonElementTypes
+    {
+        Property,
+        Object,
+        Array
+    }
+}
